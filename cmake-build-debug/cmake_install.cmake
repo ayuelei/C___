10e22bb6e21a4ba3f8524fd95b4eb294b7ab3007
YueleiHou <ninereally9@gmail.com>
2022-12-12 14:48:47 +0800

    second_commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/clion_pro/C_++
+# Install script for directory: E:/clion_pro/C___
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -32,11 +32,6 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/mingw/bin/objdump.exe")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/clion_pro/C_++/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/clion_pro/C___/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -5,14 +5,11 @@ using namespace std;        // 指定缺省的命名空间。
 // main函数，程序从这里开始执行，每个程序只能有一个main函数。
 int main()
 {
-    /*int a =10, b=20;
+    // 超女必须满足四个条件：年龄25岁以下，身高165以上、体重50公重以下，颜值要漂亮。
+    int age = 23;
+    int height = 168;
+    double weight = 48.8;
+    bool yz = true;
 
-    cout << "a=" << a << endl;
-    cout << "b=" << b << endl;*/
-
-    int a, b;
-    //b = a = 2, a * 2;
-    b = (a = 2, a * 2);
-    cout << "a=" << a << endl;
-    cout << "b=" << b << endl;
+    cout << "result="<<(age<25&&height>165&&weight<50&&yz==true) << endl;
 }
